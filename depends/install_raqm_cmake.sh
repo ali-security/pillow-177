d678df56caf2efa65ccdb898b84f0754d3991caf
Ben Bader <ben.b@sealsecurity.io>
2024-07-03 11:12:22 +0300

    deps

--- depends/install_raqm_cmake.sh
+++ depends/install_raqm_cmake.sh
@@ -3,7 +3,7 @@
 
 archive=raqm-cmake-99300ff3
 
-./download-and-extract.sh $archivehttps://raw.githubusercontent.com/python-pillow/pillow-depends/503ff2ff53e2f0446aa06a4a6c6cb17361b108e2/$archive.tar.gz
+./download-and-extract.sh $archive https://raw.githubusercontent.com/python-pillow/pillow-depends/503ff2ff53e2f0446aa06a4a6c6cb17361b108e2/$archive.tar.gz
 
 pushd $archive
 
