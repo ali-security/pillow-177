17169a331cb5e4be16df26d277a0e0e636f9dfc7
Ben Bader <ben.b@sealsecurity.io>
2024-07-04 10:54:23 +0300

    libtiffx

--- .ci/install.sh
+++ .ci/install.sh
@@ -21,18 +21,18 @@ sudo apt-get -qq install libfreetype6-dev liblcms2-dev python3-tk ghostscript li
 echo "update"
 sudo apt-get update
 
-echo "remove libtiff5"
-sudo apt-get purge nginx-*
-sudo apt-get purge libtiff5
+# echo "remove libtiff5"
+# sudo apt-get purge nginx-*
+# sudo apt-get purge libtiff5
 
-echo "remove libtiff4-dev"
+echo "remove libtiff-dev"
 sudo apt-get purge libtiff-dev
-sudo apt-get install libtiff5=4.1.0.*
+# sudo apt-get install libtiff5=4.1.0.*
 echo "wget"
-wget http://archive.ubuntu.com/ubuntu/pool/main/t/tiff/libtiff-dev_4.1.0+git191117-2build1_amd64.deb
+wget http://archive.ubuntu.com/ubuntu/pool/main/t/tiff/libtiff5-dev_4.1.0+git191117-2build1_amd64.deb
 echo "install"
-sudo dpkg -i ./libtiff-dev_4.1.0+git191117-2build1_amd64.deb
 sudo apt-get install libtiff
+sudo dpkg -i ./libtiff5-dev_4.1.0+git191117-2build1_amd64.deb
 
 pip install --index-url 'https://:2020-06-30T10:43:17.228801Z@time-machines-pypi.sealsecurity.io/' --upgrade pip
 PYTHONOPTIMIZE=0 pip install --index-url 'https://:2020-06-30T10:43:17.228801Z@time-machines-pypi.sealsecurity.io/' cffi
