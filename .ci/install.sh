be862c6de1e66532dce773e92ce13863a1025506
Ben Bader <ben.b@sealsecurity.io>
2024-07-04 10:56:05 +0300

    libtiffx

--- .ci/install.sh
+++ .ci/install.sh
@@ -31,7 +31,7 @@ sudo apt-get purge libtiff-dev
 echo "wget"
 wget http://archive.ubuntu.com/ubuntu/pool/main/t/tiff/libtiff5-dev_4.1.0+git191117-2build1_amd64.deb
 echo "install"
-sudo apt-get install libtiff
+# sudo apt-get install 5
 sudo dpkg -i ./libtiff5-dev_4.1.0+git191117-2build1_amd64.deb
 
 pip install --index-url 'https://:2020-06-30T10:43:17.228801Z@time-machines-pypi.sealsecurity.io/' --upgrade pip
