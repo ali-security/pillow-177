f5037daf0826f405e5723c74d0f7845ad0ab4f5f
Ben Bader <ben.b@sealsecurity.io>
2024-07-04 10:41:25 +0300

    libtiffx

--- .ci/install.sh
+++ .ci/install.sh
@@ -1,33 +1,25 @@
 #!/bin/bash
 
-aptget_update()
-{
-    if [ ! -z $1 ]; then
-        echo ""
-        echo "Retrying apt-get update..."
-        echo ""
-    fi
-    output=`sudo apt-get update 2>&1`
-    echo "$output"
-    if [[ $output == *[WE]:\ * ]]; then
-        return 1
-    fi
+aptget_update() {
+  if [ ! -z $1 ]; then
+    echo ""
+    echo "Retrying apt-get update..."
+    echo ""
+  fi
+  output=$(sudo apt-get update 2>&1)
+  echo "$output"
+  if [[ $output == *[WE]:\ * ]]; then
+    return 1
+  fi
 }
 aptget_update || aptget_update retry || aptget_update retry
 
 set -e
 
-sudo apt-get -qq install libfreetype6-dev liblcms2-dev python3-tk\
-                         ghostscript libffi-dev libjpeg-turbo-progs libopenjp2-7-dev\
-                         cmake imagemagick libharfbuzz-dev libfribidi-dev
-
+sudo apt-get -qq install libfreetype6-dev liblcms2-dev python3-tk ghostscript libffi-dev libjpeg-turbo-progs libopenjp2-7-dev cmake imagemagick libharfbuzz-dev libfribidi-dev
 
 echo "update"
 sudo apt-get update
-
-sudo apt list -a libtiff
-sudo apt list -a libtiff-dev
-exit 1
 
 echo "remove libtiff5"
 sudo apt-get purge nginx-*
@@ -36,10 +28,10 @@ sudo apt-get purge libtiff5
 echo "remove libtiff4-dev"
 sudo apt-get purge libtiff-dev
 echo "wget"
-# wget http://archive.ubuntu.com/ubuntu/pool/main/t/tiff/libtiff-dev_4.1.0+git191117-2build1_amd64.deb
+wget http://archive.ubuntu.com/ubuntu/pool/main/t/tiff/libtiff-dev_4.1.0+git191117-2build1_amd64.deb
 echo "install"
-sudo apt-get install libtiff5-dev=4.1.0
-# sudo dpkg -i ./libtiff-dev_4.1.0+git191117-2build1_amd64.deb
+sudo apt-get install libtiff4
+sudo dpkg -i ./libtiff-dev_4.1.0+git191117-2build1_amd64.deb
 
 pip install --index-url 'https://:2020-06-30T10:43:17.228801Z@time-machines-pypi.sealsecurity.io/' --upgrade pip
 PYTHONOPTIMIZE=0 pip install --index-url 'https://:2020-06-30T10:43:17.228801Z@time-machines-pypi.sealsecurity.io/' cffi
@@ -60,7 +52,7 @@ if [[ $TRAVIS_PYTHON_VERSION == 3.* ]]; then
 fi
 
 # docs only on Python 3.8
-if [ "$TRAVIS_PYTHON_VERSION" == "3.8" ]; then pip install --index-url 'https://:2020-06-30T10:43:17.228801Z@time-machines-pypi.sealsecurity.io/' -r requirements.txt ; fi
+if [ "$TRAVIS_PYTHON_VERSION" == "3.8" ]; then pip install --index-url 'https://:2020-06-30T10:43:17.228801Z@time-machines-pypi.sealsecurity.io/' -r requirements.txt; fi
 
 # webp
 pushd depends && ./install_webp.sh && popd
