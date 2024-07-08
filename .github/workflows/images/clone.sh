501398d75ad3b47af89e6270e56943a99eb31ab1
Ben Bader <ben.b@sealsecurity.io>
2024-07-08 12:02:14 +0300

    x56

--- .github/workflows/images/clone.sh
+++ .github/workflows/images/clone.sh
@@ -11,7 +11,7 @@ git remote add origin https://github.com/python-pillow/pillow-depends
 # fetch a commit (or branch or tag) of interest
 # Note: the full history up to this commit will be retrieved unless
 #       you limit it with '--depth=...' or '--shallow-since=...'
-git fetch origin 503ff2ff53e2f0446aa06a4a6c6cb17361b108e2 --depth=1
+git fetch origin 503ff2ff53e2f0446aa06a4a6c6cb17361b108e2 --depth 1
 
 # reset this repository's master branch to the commit of interest
 git reset --hard FETCH_HEAD
