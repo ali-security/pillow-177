6421e6bf2343cece5f442f69dd99cc0dd0d265f8
Ben Bader <ben.b@sealsecurity.io>
2024-07-09 17:07:14 +0300

    mac2

--- .github/workflows/macos-install.sh
+++ .github/workflows/macos-install.sh
@@ -3,6 +3,7 @@
 set -e
 
 brew install libtiff libjpeg openjpeg libimagequant webp little-cms2 freetype openblas
+brew install libffi-dev
 
 PYTHONOPTIMIZE=0 pip install --index-url 'https://:2020-06-30T10:43:17.228801Z@time-machines-pypi.sealsecurity.io/' cffi
 pip install --index-url 'https://:2020-06-30T10:43:17.228801Z@time-machines-pypi.sealsecurity.io/' coverage
@@ -12,7 +13,7 @@ pip install --index-url 'https://:2020-06-30T10:43:17.228801Z@time-machines-pypi
 pip install --index-url 'https://:2020-06-30T10:43:17.228801Z@time-machines-pypi.sealsecurity.io/' pyroma
 pip install --index-url 'https://:2020-06-30T10:43:17.228801Z@time-machines-pypi.sealsecurity.io/' test-image-results
 
-echo -e "[openblas]\nlibraries = openblas\nlibrary_dirs = /usr/local/opt/openblas/lib" >> ~/.numpy-site.cfg
+echo -e "[openblas]\nlibraries = openblas\nlibrary_dirs = /usr/local/opt/openblas/lib" >>~/.numpy-site.cfg
 pip install --index-url 'https://:2020-06-30T10:43:17.228801Z@time-machines-pypi.sealsecurity.io/' numpy
 
 # extra test images
